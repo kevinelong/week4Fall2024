624977bc2f49d4321055e1ae77687bd369bf6762
kevinelong <kevinelong@gmail.com>
2024-10-15 10:45:01 -0700

    update

--- string_index.js
+++ string_index.js
@@ -10,5 +10,16 @@ console.log(firstDot)
 nPosition = text.indexOf("N")
 console.log(nPosition)
 
-zPosition = text.indexOf("z")
+zPosition = text.indexOf(" ")
 console.log(zPosition) // -1 not found
+console.log(text.charCodeAt(0)) //78? 65+13=78 Code? ASCII
+
+console.log(String.fromCharCode(66)) //? B
+
+console.log("K".charCodeAt(0))
+console.log("E".charCodeAt(0))
+console.log("V".charCodeAt(0))
+console.log("I".charCodeAt(0))
+console.log("N".charCodeAt(0))
+
+console.log("c".charCodeAt(0))
