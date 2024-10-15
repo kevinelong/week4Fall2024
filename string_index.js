5cbbdf69526eae812fd54400547e0c36a52b3b13
kevinelong <kevinelong@gmail.com>
2024-10-15 10:28:07 -0700

    string_index

---
+++ string_index.js
@@ -0,0 +1,14 @@
+//                1111111111222222222233333333
+//      01234567890123456789012345678901234567
+text = "Now is the time for all good people..."
+index = text.indexOf(" ")
+console.log(index)
+console.log(text.length)
+firstDot  = text.indexOf(".")
+console.log(firstDot)
+
+nPosition = text.indexOf("N")
+console.log(nPosition)
+
+zPosition = text.indexOf("z")
+console.log(zPosition) // -1 not found
